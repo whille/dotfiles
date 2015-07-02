4898f02b2892effefc7b541b305e4b0825026d4b
nathon.wang <nathon.wang713@gmail.com>
2015-07-02 14:31:59 +0800

    bootup

--- bootup.sh
+++ bootup.sh
@@ -7,13 +7,16 @@ mkdir -p ${LILYTERM_HOME}
 rm -rf ${LILYTERM_HOME}/default.conf
 ln -s ${DOTFILES_HOME}/lilyterm.conf ${LILYTERM_HOME}/default.conf
 
+echo 'Installing zsh config....'
 if [[ ! -f ${HOME}/.zshrc ]]; then
     ln -s ${DOTFILES_HOME}/zshconfig/zshrc ${HOME}/.zshrc
     source ~/.zshrc
 else
     echo "zsh init failed because ~/.zshrc is exists, delete it first!!!"
+    exit -1
 fi
 
+echo 'Installing vim config....'
 if [[ ! -f ${HOME}/.vimrc ]]; then
     if [[ ! -d ${VIM_BUNDLE_HOME} ]]; then
         git clone https://github.com/gmarik/vundle.git ${VIM_BUNDLE_HOME}
@@ -24,5 +27,9 @@ if [[ ! -f ${HOME}/.vimrc ]]; then
     viminstall
 else
     echo "vim init failed because ~/.vimrc is exists, delete it first!!!"
+    exit -1
 fi
+
+echo 'Installing git config....'
+ln -s ${HOME}/gitconfig ${DOTFILES_HOME}/gitconfig
 
